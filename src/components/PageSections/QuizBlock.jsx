026d64a9439e91b6bd657a25779d1de3b922a0f0
Mollie Bourke <zn011920@student.reading.ac.uk>
2021-01-21 12:29:22 +0000

    Remove redundant constructor

--- src/components/PageSections/QuizBlock.jsx
+++ src/components/PageSections/QuizBlock.jsx
@@ -2,10 +2,6 @@ import React, {Component} from 'react';
 import './PageSections.css'
 
 class QuizBlock extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div>
