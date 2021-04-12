441ae71bd03a884c8f036e882c2fd96747c514f2
Mollie Bourke <zn011920@student.reading.ac.uk>
2021-04-12 14:48:32 +0100

    Final fixes before UI changes

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -11,50 +11,38 @@ class Sidebar extends Component {
                 {
                     title: 'Home',
                     path: '/',
-                    icon: <i className='fas fa-home'/>,
+                    icon: 'fas fa-home',
                 },
                 {
                     title: 'CVs and Cover Letters',
                     path: '/cvs_and_cover_letters',
-                    icon: <i className='fas fa-mail-bulk'/>,
+                    icon: 'fas fa-mail-bulk',
                 },
                 {
                     title: 'Psychometric Tests',
                     path: '/psychometric_tests',
-                    icon: <i className='fas fa-chalkboard-teacher'/>,
+                    icon: 'fas fa-chalkboard-teacher',
                 },
                 {
                     title: 'Assessment Centres',
                     path: '/assessment_centres',
-                    icon: <i className='fas fa-briefcase'/>,
+                    icon: 'fas fa-briefcase',
                 },
                 {
                     title: 'Interviews',
                     path: '/interviews',
-                    icon: <i className="fas fa-handshake"/>,
+                    icon: 'fas fa-handshake',
                 },
                 {
                     title: 'Industry Skills',
                     path: '/industry_skills',
-                    icon: <i className="fas fa-industry"/>,
+                    icon: 'fas fa-industry',
                 }
             ]
         };
     }
 
     render() {
-        // let pages;
-        //
-        // axios({
-        //     method: 'GET',
-        //     url: `${API_PATH}/get_sidebar_items.php`,
-        //     headers: {'content-type': 'application/json'}
-        // })
-        //     .then(results=> {
-        //         pages = results;
-        //     })
-        //     .catch(error => this.setState({error:error.message}))
-
         return (
             <div className='side-menu' style={{width: this.state.sidebar ? 280 : 80}}>
                 <ul>
@@ -67,7 +55,7 @@ class Sidebar extends Component {
                     {this.state.pages.map((item, index) => (
                         <li key={index} className='side-text'>
                             <Link to={item.path}>
-                                {item.icon}
+                                <i className={item.icon}/>
                                 {this.state.sidebar ? <span>{item.title}</span> : <span/>}
                             </Link>
                         </li>
