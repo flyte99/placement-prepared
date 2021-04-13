292b646acfdf27db176dd12c355ae9e7ddb9d735
Mollie Bourke <zn011920@student.reading.ac.uk>
2021-04-13 16:38:59 +0100

    Test old index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 ), document.getElementById('root'));
-
-serviceWorker.unregister();
