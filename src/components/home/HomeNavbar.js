5f0188d37fb7ac7c0c85606a2799cf2c587de420
Mollie Bourke <zn011920@student.reading.ac.uk>
2021-04-16 00:13:25 +0100

    Update design

--- src/components/home/HomeNavbar.js
+++ src/components/home/HomeNavbar.js
@@ -1,45 +1,29 @@
-import { useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import {
-  AppBar,
-  Badge,
-  Box,
-  Hidden,
-  IconButton,
-  Toolbar
-} from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
+import { AppBar, Box, Hidden, IconButton, Toolbar, Typography } from '@material-ui/core';
 import InputIcon from '@material-ui/icons/Input';
+import MenuIcon from '@material-ui/icons/Menu';
+import PropTypes from 'prop-types';
+import { Link as RouterLink } from 'react-router-dom';
 import Logo from '../Logo';
 
-const HomeNavbar = ({ onMobileNavOpen, ...rest }) => {
-  const [notifications] = useState([]);
-
-  return (
-    <AppBar
-      elevation={0}
-      {...rest}
-    >
-      <Toolbar>
-        <RouterLink to="/"><Logo /></RouterLink>
-        <Box sx={{ flexGrow: 1 }} />
-        <Hidden lgDown>
-          <IconButton color="inherit">
-            <Badge badgeContent={notifications.length} color="primary" variant="dot">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <IconButton color="inherit"><InputIcon /></IconButton>
-        </Hidden>
-        <Hidden lgUp>
-          <IconButton color="inherit" onClick={onMobileNavOpen}><MenuIcon /></IconButton>
-        </Hidden>
-      </Toolbar>
-    </AppBar>
-  );
-};
+const HomeNavbar = ({ onMobileNavOpen, ...rest }) => (
+  <AppBar
+    elevation={0}
+    {...rest}
+  >
+    <Toolbar>
+      <RouterLink to="/"><Logo /></RouterLink>
+      <Typography sx={{ ml: 2 }} variant="h1">Placement Prepared</Typography>
+      <Box sx={{ flexGrow: 1 }} />
+      <Hidden lgDown>
+        {localStorage.getItem('token') ? (
+          <IconButton color="inherit" onClick={localStorage.clear()}><InputIcon /></IconButton>) : null}
+      </Hidden>
+      <Hidden lgUp>
+        <IconButton color="inherit" onClick={onMobileNavOpen}><MenuIcon /></IconButton>
+      </Hidden>
+    </Toolbar>
+  </AppBar>
+);
 
 HomeNavbar.propTypes = {
   onMobileNavOpen: PropTypes.func
