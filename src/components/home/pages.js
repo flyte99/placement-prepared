e7c757515c8706c0bd5f740d8287991a72a18343
Mollie Bourke <zn011920@student.reading.ac.uk>
2021-04-25 12:28:54 +0100

    Remove nested routing

--- src/components/home/pages.js
+++ src/components/home/pages.js
@@ -11,9 +11,9 @@ export default [
     icon: FileText,
     path: 'cvs_and_cover_letters',
     subpages: [
-      { title: 'Writing a CV', path: 'cvs_and_cover_letters/cv' },
-      { title: 'Cover Letters', path: 'cvs_and_cover_letters/cover-letters' },
-      { title: 'LinkedIn', path: 'cvs_and_cover_letters/LinkedIn' }
+      { title: 'Writing a CV', path: 'writing_a_cv' },
+      { title: 'Cover Letters', path: 'cover_letters' },
+      { title: 'LinkedIn', path: 'linkedin' }
     ]
   },
   {
@@ -21,8 +21,8 @@ export default [
     icon: PenTool,
     path: 'psychometric_testing',
     subpages: [
-      { title: 'Situational Judgement', path: 'psychometric_testing/situational_judgement' },
-      { title: 'Personality Assessments', path: 'psychometric_testing/personality' }
+      { title: 'Situational Judgement', path: 'situational_judgement_tests' },
+      { title: 'Personality Assessments', path: 'personality_assessments' }
     ]
   },
   {
@@ -30,9 +30,9 @@ export default [
     icon: Briefcase,
     path: 'assessment_centres',
     subpages: [
-      { title: 'Group Exercises', path: 'assessment_centres/group_exercises' },
-      { title: 'Virtual Assessment Centres', path: 'assessment_centres/virtual' },
-      { title: 'Presentations', path: 'assessment_centres/presentations' }
+      { title: 'Group Exercises', path: 'group_exercises' },
+      { title: 'Virtual Assessment Centres', path: 'virtual_assessment_centres' },
+      { title: 'Presentations', path: 'presentations' }
     ]
   },
   {
@@ -40,9 +40,9 @@ export default [
     icon: Users,
     path: 'interviews',
     subpages: [
-      { title: 'Telephone Interviews', path: 'interviews/telephone' },
-      { title: 'Video Interviews', path: 'interviews/video' },
-      { title: 'Face-to-Face Interviews', path: 'interviews/face-to-face' }
+      { title: 'Telephone Interviews', path: 'telephone_interviews' },
+      { title: 'Video Interviews', path: 'video_interviews' },
+      { title: 'Face-to-Face Interviews', path: 'face_to_face_interviews' }
     ]
   },
   {
@@ -50,10 +50,10 @@ export default [
     icon: Codesandbox,
     path: 'industry_skills',
     subpages: [
-      { title: 'Agile Development Methods', path: 'industry_skills/agile_development' },
-      { title: 'Git', path: 'industry_skills/git' },
-      { title: 'MySQL', path: 'industry_skills/mysql' },
-      { title: 'Unit Testing', path: 'industry_skills/unit_testing' }
+      { title: 'Agile Development Methods', path: 'agile_development_methods' },
+      { title: 'Git', path: 'git' },
+      { title: 'MySQL', path: 'mysql' },
+      { title: 'Unit Testing', path: 'unit_testing' }
     ]
   },
   {
